3d15b3c299c919080aefe23b70195bc167f39223
Basavaraju-MN <basavaraju.m.n22@gmail.com>
2026-08-19 15:20:09 +0530

    Update server.js

--- apps/server/src/server.js
+++ apps/server/src/server.js
@@ -1,34 +1,28 @@
 const express = require('express');
 const cors = require('cors');
-
+ 
 const trekRoutes = require('./routes/routes');
-
+ 
 const app = express();
-const path = require('path');
-
-//Third-party libraries
-
-const cookieParser = require('cookie-parser');
-const routes = require('./routes/routes');
-
-// Routes
-app.use('/api/phonepe', routes);
-
+ 
+const PORT = 4000;
+ 
 app.use(cors({
-  origin: 'http://localhost:5173',
+  origin: 'http://localhost:5173' || process.env.CLIENT_URL || 'http://localhost:5174',
+  credentials: true,
 }));
-
+ 
 app.use(express.json());
-
+ 
 app.get('/', (req, res) => {
   res.json({
     success: true,
     message: 'Node.js server is running',
   });
 });
-
+ 
 app.use('/api', trekRoutes);
-
+ 
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
